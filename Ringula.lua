b995f163f41d23c315d1e8ffde674913fd627379
Showerduck <showerduck900@gmail.com>
2019-05-27 19:02:07 +0300

    Pohja luotu Ringulalle.

--- Ringula.lua
+++ Ringula.lua
@@ -1,4 +1,21 @@
+local isOpen = false
+
+
+Ringula_settings =
+{
+
+    NumButtonCount = 8, 
+    StartPage = 13,
+
+}
+
+
+
+
 function RingulaFrame_OnLoad()
+CloseRingu_Menu()
+
+this:RegisterEvent("VARIABLES_LOADED")
 
 
 
@@ -8,7 +25,10 @@ end
 
 function RingulaFrame_OnEvent(event)
 
+    if event == "VARIABLES_LOADED"
+    ConfigureButtons()
 
+    end
 
 end
 
@@ -17,5 +37,44 @@ end
 function RingulaFrame_OnUpdate(arg1)
 
 
+
+
+
+end
+
+function ToggleRingu_Menu()
+
+    if isOpen then 
+
+    else
+
+    end
+
+end
+
+function CloseRingu_Menu()
+
+
+end
+
+function OpenRingu_Menu ()
+
+
+end
+
+
+function RingMenu_GetMousePosition()
+    local mouseX, mouseY = GetCursorPosition()
+    local uiScale = RingMenuFrame:GetParent():GetEffectiveScale()
+    mouseX = mouseX / uiScale
+    mouseY = mouseY / uiScale
+    return mouseX, mouseY
+end
+
+function ConfigureButtons ()
+
+    for i = 1, Ringula_settings.NumButtonCount do
+        
+    end
 
 end
