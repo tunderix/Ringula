1889e56f46d08b8e60eab362d1831ac2af8726f1
Sampsoni <sami.repo@live.com>
2019-05-29 21:39:28 +0300

    Action Buttons Following Mouse.

--- Ringula.lua
+++ Ringula.lua
@@ -56,7 +56,6 @@ function RingulaFrame_OnEvent(event)
     if event == "VARIABLES_LOADED" then
         Ringula_LoadNewDefaultSettings()
         CreateRingulaFrame()
-
         -- Setup Settings for Ringula. file: RingulaSettings.lua
         RingulaSettings_SetupSettingsFrame()
     end
@@ -72,6 +71,46 @@ function RingulaFrame_OnUpdate(elapsed)
     end
 end
 
+function CreateRingulaFrame ()
+
+
+    for i = 1, 8 do
+        local buttonName = "Nappula" .. i
+        local button = getglobal(buttonName) 
+        if not button then 
+            button = CreateFrame("CheckButton", buttonName, RingulaFrame, "BonusActionButtonTemplate")
+            -- Hide Hotkey text
+            local hotkey = getglobal(buttonName .. "HotKey")
+            hotkey:Hide()
+            -- Hook individual button callbacks
+            button.oldScriptOnClick = button:GetScript("OnClick")
+            button:SetScript("OnClick", RingulaOnClick)
+            button.oldScriptOnEnter = button:GetScript("OnEnter")
+            button:SetScript("OnEnter", RingulaOnEnter)
+            
+        end
+        button:SetID(1020 + i) -- TODO : WHAT ID...May conflict with other addons.
+        button:SetPoint("CENTER", RingulaFrame, "CENTER", 0, 0)
+        button:SetFrameLevel(2)
+        button.isRingula = true
+        button.isBonus = true
+        button.buttonType = "RINGULA_MENU" --to do: Varmista, mihin tämä liittyy :D--
+       
+        -- local icon = getglobal(buttonName .. "Icon")
+        -- icon:SetTextCoord (0.0, 1.0, 0.0, 1.0) 
+        button:Enable()
+        button:Show()
+
+        this = button
+       -- ActionButton_Update() 
+
+       message("Button " .. buttonName .. "position is: " )
+
+    end
+
+    Ringula_UpdateButtonPositions()
+end
+
 function Ringula_UpdateButtonPositions()
 
     local radius = 100.0 --TODO Change these into default_settings
@@ -80,15 +119,13 @@ function Ringula_UpdateButtonPositions()
     local currentSize = 1
 
      -- Button positions
-     
-    
-         for i = 1, RingulaSettings.NumButtonCount do
+     for i = 1, RingulaSettings.NumButtonCount do
          local buttonName = "Nappula" .. i
          local button = getglobal(buttonName) 
          local angle = angleOffsetRadians + 2.0 * math.pi * (i - 1) / RingulaSettings.NumButtonCount
          local buttonX = radius * math.sin(angle)
          local buttonY = radius * math.cos(angle)
-         button:SetPoint("CENTER", RingMenuFrame, "CENTER", buttonX, buttonY)
+         button:SetPoint("CENTER", RingulaFrame, "CENTER", buttonX, buttonY)
          button:SetAlpha(200)
      end
 
@@ -148,43 +185,6 @@ function Ringula_GetMousePosition()
     return mouseX, mouseY
 end
 
-function CreateRingulaFrame ()
-
-
-    for i = 1, RingulaSettings.NumButtonCount do
-        local buttonName = "Nappula" .. i
-        local button = getglobal(buttonName) 
-        if not button then 
-            button = CreateFrame("CheckButton", buttonName, RingulaFrame, "BonusActionButtonTemplate")
-            -- Hide Hotkey text
-            local hotkey = getglobal(buttonName .. "HotKey")
-            hotkey:Hide()
-            -- Hook individual button callbacks
-            button.oldScriptOnClick = button:GetScript("OnClick")
-            button:SetScript("OnClick", RingulaOnClick)
-            button.oldScriptOnEnter = button:GetScript("OnEnter")
-            button:SetScript("OnEnter", RingulaOnEnter)
-            
-        end
-        button:SetID(i)
-        button:SetPoint("CENTER", RingulaFrame, "CENTER", 0, 0)
-        button:SetFrameLevel(2)
-        button.isRingula = true
-        button.isBonus = true
-        button.buttonType = "RINGULA_MENU" --to do: Varmista, mihin tämä liittyy :D--
-       
-        -- local icon = getglobal(buttonName .. "Icon")
-        -- icon:SetTextCoord (0.0, 1.0, 0.0, 1.0) 
-        button:Enable()
-        button:Show()
-
-        this = button
-       -- ActionButton_Update() 
-
-
-    end
-
-end
 
 function RingulaOnClick ()
 
